77e40d6815bd3c6a1351e6acc80f6daf5cbb2e02
antonysa23-meet <antony.saleh2017@gmail.com>
2025-12-01 15:31:25 -0600

    Fix case-sensitive import for sponsor.css

--- components/10/sponsor/sponsor.js
+++ components/10/sponsor/sponsor.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { Box, Flex, Image } from 'rebass';
-import './Sponsor.css';
+import './sponsor.css';
 import { ourSponsors } from './sponsors.json';
 
 const tiers = [
